650504b705d95b6b5ced8d5ec83ca589c47afd3e
TiennLevii <nguyentrachtien2401@gmail.com>
2024-06-15 14:19:57 +0700

    fix again

--- src/services/channel.ts
+++ src/services/channel.ts
@@ -1,11 +1,8 @@
 import baseUrl from "@/config/axios";
-import { ApiKey } from "@/constants";
 
 export const getChannels = async (id: string) => {
   try {
-    const response = await baseUrl.get(
-      `/channels?part=snippet&id=${id}&key=${ApiKey}`
-    );
+    const response = await baseUrl.get("");
     return response.data;
   } catch (error) {
     console.log(error);
