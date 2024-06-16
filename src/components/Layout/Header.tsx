5ccd17c4f446bc876a0fd6cc06844ba9736d2631
TiennLevii <nguyentrachtien2401@gmail.com>
2024-06-16 19:42:48 +0700

    fix: search

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,21 +1,16 @@
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
 import logo from "../../../public/Logo.png";
 import { Button } from "../Button/Button";
 import { useSidebarContext } from "../../context/SidebarContext";
-import { useRouter } from "next/navigation";
-import { searchQuery } from "@/services/query";
 
 export function Header() {
-  const [data, setData] = useState({});
   const [value, setValue] = useState<string>("");
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
-  const router = useRouter();
 
   const handleSearch = async (e?: FormEvent) => {
     e?.preventDefault();
-    router.push(`/search?q=${encodeURIComponent(value || "")}`);
-    router.refresh();
+    window.location.href = `/search?q=${value}`;
   };
 
   return (
--- src/hooks/useSearchQuery.ts
+++ src/hooks/useSearchQuery.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { searchQuery } from "@/services/query";
 import { useSearchParams } from "next/navigation";
@@ -6,26 +5,14 @@ import { useSearchParams } from "next/navigation";
 function useSearchQuery() {
   const searchParams = useSearchParams();
   const search: any = searchParams ? searchParams.get("q") : null;
-  const { data, isLoading, ...rest } = useQuery({
+  const { data, ...rest } = useQuery({
     queryKey: ["data"],
     queryFn: async () => {
       return await searchQuery(search);
     },
   });
 
-  useEffect(() => {
-    const handler = setTimeout(async () => {
-      const result = await searchQuery(search);
-      console.log(result);
-      return result;
-    }, 1000);
-
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [search]);
-
-  return { data, isLoading, ...rest };
+  return { data, ...rest };
 }
 
 export default useSearchQuery;
